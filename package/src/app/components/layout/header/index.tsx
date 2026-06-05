58a6e3aca12d1b2eecdac48074f267aea80c6974
AndresOrdonez369 <oswaldordonez@unicauca.edu.co>
2026-06-05 13:14:54 -0500

    Update CV

--- package/src/app/components/layout/header/index.tsx
+++ package/src/app/components/layout/header/index.tsx
@@ -5,8 +5,21 @@ import Logo from "../logo";
 
 const Header = () => {
   const handleDownloadPDF = () => {
-    window.print();
+    // 1. Crea un elemento de enlace <a> invisible
+    const link = document.createElement("a");
+    
+    // 2. Define la ruta hacia tu archivo en la carpeta public
+    link.href = "/images/work/CVJenniferVelasquez.pdf";
+    
+    // 3. Agrega el atributo de descarga y el nombre con el que se guardará
+    link.download = "CV_Jennifer_Velasquez.pdf"; 
+    
+    // 4. Añade el enlace al documento, simula el clic y luego elimínalo
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
   };
+
   return (
     <header className="navbar top-0 left-0 z-999 w-full absolute">
       <div className="container">
@@ -17,7 +30,7 @@ const Header = () => {
             </div>
 
             <Button
-              variant={"outline"}
+              variant="outline"
               onClick={handleDownloadPDF}
               className="relative overflow-hidden cursor-pointer w-fit h-full py-2 sm:py-3 md:py-5 px-4 sm:px-5 md:px-7 border border-primary rounded-full group"
             >
